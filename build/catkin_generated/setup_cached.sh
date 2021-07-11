ccc653bd477519d1993e97d55d139a82ddf57b45
Karan <pyrotank41@gmail.com>
2021-07-10 22:21:07 -0700

    updated name of the ros package

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/karan-vm/catkin_ws/src/sim_drone/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/karan-vm/catkin_ws/src/sim_drone/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/melodic/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/karan-vm/.local/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/karan-vm/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/karan-vm/catkin_ws/src/sim_drone/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/karan-vm/catkin_ws/src/sim_drone/build'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/karan-vm/catkin_ws/src/sim_drone/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH='/home/karan-vm/catkin_ws/src/sim_drone:/home/karan-vm/catkin_ws/src/mavlink:/home/karan-vm/catkin_ws/src/mavros/libmavconn:/home/karan-vm/catkin_ws/src/mavros/mavros_msgs:/home/karan-vm/catkin_ws/src/mavros/mavros:/home/karan-vm/catkin_ws/src/mavros/mavros_extras:/home/karan-vm/catkin_ws/src/ros_numpy:/home/karan-vm/catkin_ws/src/sim_drone:/home/karan-vm/catkin_ws/src/mavros/test_mavros:/home/karan-vm/catkin_ws/src/v2_sim_drone:/opt/ros/melodic/share'
+export ROS_PACKAGE_PATH='/home/karan-vm/catkin_ws/src/sim_drone:/home/karan-vm/catkin_ws/src/mavlink:/home/karan-vm/catkin_ws/src/mavros/libmavconn:/home/karan-vm/catkin_ws/src/mavros/mavros_msgs:/home/karan-vm/catkin_ws/src/mavros/mavros:/home/karan-vm/catkin_ws/src/mavros/mavros_extras:/home/karan-vm/catkin_ws/src/ros_numpy:/home/karan-vm/catkin_ws/src/sim_drone_bkp:/home/karan-vm/catkin_ws/src/mavros/test_mavros:/home/karan-vm/catkin_ws/src/sim_drone:/opt/ros/melodic/share'
