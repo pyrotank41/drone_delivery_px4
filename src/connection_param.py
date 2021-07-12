9c623cccd3ac95728e0c7cf8c502c6c4c5138512
karan <pyrotank41@gmail.com>
2021-07-12 09:06:06 -0400

    merging

--- src/connection_param.py
+++ src/connection_param.py
@@ -1,5 +1,6 @@
 
-connection_string = 'udp://:14540'
+#connection_string = 'udp://:14540'
+connection_string = 'serial:////dev/ttyPixhawk:115200'
 # "serial:////dev/ttyACM0" example to connect to serial port
 maximum_land_speed = 10
 ros_camera_topic = '/solo/pi_cam_v2/image_raw'
